03173973f81b6b228b26bf5b037378782d726509
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2021-08-31 12:26:54 +0100

    example

--- examples/sendTaskToCelery.py
+++ examples/sendTaskToCelery.py
@@ -1,6 +1,7 @@
 from celery import Celery
 
-app = Celery('tasks', broker="redis://mgnify-sourmash:6379/0", backend="redis://mgnify-sourmash:6379/1")
-r = app.send_task('tasks.run_gather', ('q1.sig',))
+app = Celery('tasks', broker="redis://localhost:6379/0", backend="redis://localhost:6379/1")
+r = app.send_task('tasks.run_gather', ('8a2b9f64bb476965359344d1fb83a6b1.sig', 'JAFQLC01.fasta', 'HGUT'))
+
 print(f"Task ID: {r.id}")
 print(f"STATUS: {r.status}")
